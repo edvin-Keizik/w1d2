11f99959150dcf81ca2743fbf75012c36be849df
Edwinofer <e.keizik@visma.com>
2026-02-10 10:24:19 +0200

    add cache

---
+++ AnagramSolver.BuisnessLogic/MemoryCache.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramSolver.BusinessLogic
+{
+    internal class MemoryCache<T>
+    {
+        private Dictionary<string, T> _cache = new();
+
+        public bool TryGetCache(string key, out T value)
+        {
+            return _cache.TryGetValue(key, out value);
+        }
+
+        public void AddCache(string key, T value)
+        {
+            _cache[key] = value;
+        }
+    }
+}
